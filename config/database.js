2e98775efcd45ba572acac6dc0c6536cd67b1a3c
Ubuntu <ubuntu@ip-172-31-10-45.us-west-1.compute.internal>
2016-05-19 21:42:21 +0000

    Final Commit 2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://ec2-52-53-197-214.us-west-1.compute.amazonaws.com:27017/dummyDB'
+	url: 'mongodb://ec2-52-53-197-214.us-west-1.compute.amazonaws.com:27017/admin'
 }
