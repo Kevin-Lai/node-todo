70611e46c44f0d2d50809b37296717e1cae0b549
Ubuntu <ubuntu@ip-172-31-10-45.us-west-1.compute.internal>
2016-05-19 21:48:44 +0000

    Final Commit 3

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,7 +1,23 @@
 var mongoose = require('mongoose');
+var TodoSchema = new mongoose.Schema({
+// we define a property for this model object
+// then what type it is, in this case, the
+// completed property will has to be a Boolean
+completed: Boolean,
+// we can add validations too
+// just use an object literal here instead
+// just be sure to have a type property on that object
+// to tell mongoose what type this property will be
+text: {
+type: String, // will be a string
+required: true
+} });
 
-var TodoSchema =  new mongoose.Schema({
-   text : {type : String, default: ''}
-});
-
-module.exports = mongoose.model('Todo', TodoSchema);
+// no matter what we pass in as a name for the model,
+// mongoose will lowercase it and pluralize it for the collection.
+// so below the name for the model is 'Todo', mongoose will
+// convert that to 'todos' in the databse.
+// TodoModel is the model we'll use in node to CRUD so
+// it makes sense to export this;
+var TodoModel = mongoose.model('Todo',TodoSchema);
+module.export = TodoModel;
